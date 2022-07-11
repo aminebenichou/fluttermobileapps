4f2655f33f2d9057480cdd4273f7085c93776b8c
aminebenichou <31164395+aminebenichou@users.noreply.github.com>
2022-07-11 12:04:22 +0100

    first and second lesson

---
+++ void main(){.dart
@@ -0,0 +1,38 @@
+void main() {
+  var person = [
+    {
+      'name': 'Farouk',
+      'age': 17,
+      'Spec': 'science',
+    },
+    {
+      'name': 'Farouk',
+      'age': 17,
+      'Spec': 'science',
+    },
+    {
+      'name': 'Farouk',
+      'age': 17,
+      'Spec': 'science',
+    },
+    {
+      'name': 'Farouk',
+      'age': 17,
+      'Spec': 'science',
+    },
+  ];
+
+  int i = 0;
+
+  do {
+    print(i);
+  } while (i<5);
+
+  /* for (i = 0; i < 5; i++) {
+    print(i);
+  }
+  while (i < 5) {
+    print(i);
+    i++;
+  } */
+}
